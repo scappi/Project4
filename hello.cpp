0c7c5c12abc83bb8117fa352c665f8c75385f05f
Stefania <cappi.stefania@gmail.com>
2014-10-30 16:01:07 -0700

    Added a default value

--- hello.cpp
+++ hello.cpp
@@ -5,8 +5,15 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
+  // int answer;
+  // cout << "enter an option " << endl;
+  // cin >> answer;
+ // cout <<"Hello, " <<  argv[10] << endl;
+  if(argc < 1)
+    cout << "Hello, user" << endl;
+  else
+    cout <<"Hello, " <<  argv[10] << endl;
 
-  cout <<"Hello, " <<  argv[10] << endl;
 
   return 0;
 }
